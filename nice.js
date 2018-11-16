81de1f639bb86cc8237554554357990f070fd721
Marian Zburlea <marianzburlea@gmail.com>
2018-11-16 23:27:19 +0000

    Using for takes a lot of time, bleahhhh!

--- nice.js
+++ nice.js
@@ -7,9 +7,20 @@ let t = process.hrtime();
 
 const niceFunction = (n = 1000000) => {
   const list = Array(n + 1).fill('').map(v => ~~(Math.random() * n + 1));
-  // console.log('Generated list', list);
-  // const uniqueValues = Array.from(new Set(list))
-  const uniqueValues = [...(new Set(list))]
+
+  // Using for takes a lot of time, bleahhhh!
+  // const uv = [];
+  // var i = 0,
+  //     ll = list.length;
+  // for (i; i < ll; i++) {
+  //   if (!uv.includes(list[i])) {
+  //     uv.push(list[i]);
+  //   }
+  // }
+
+  const uniqueValues = [...(new Set(list))];
+
+  return true;
   return uniqueValues.filter(v1 => {
     // console.log(list.filter(v2 => v2 === v1))
     return list.filter(v2 => v2 === v1).length > 1
@@ -17,5 +28,5 @@ const niceFunction = (n = 1000000) => {
 }
 const elementsThatRepeat = niceFunction(n);
 t = process.hrtime(t);
-console.log(`Execution time is ${t[0] + (~~(t[1] / 1000000)/1000)} seconds`)
+console.log(`Execution time is ${t[0] + (~~(t[1] / 100000)/10000)} seconds`)
 console.log('*************************************\nNumber of elements that repeat: %s', elementsThatRepeat.length);
