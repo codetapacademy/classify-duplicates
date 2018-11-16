88381f3d961d95d7bd8de4b6f4cf9f477a5406ee
Marian Zburlea <marianzburlea@gmail.com>
2018-11-16 22:53:43 +0000

    pass n from the command line

--- nice.js
+++ nice.js
@@ -1,8 +1,10 @@
-process.stdout.write('\033c');
+// process.stdout.write('\033c');
+const n = process.argv && +process.argv[2].split('=')[1];
+
 // I want to build a function that returns an array 
 // with N + 1 elements that are random and integers
 let t = process.hrtime();
-console.log(t)
+
 const niceFunction = (n = 1000000) => {
   const list = Array(n + 1).fill('').map(v => ~~(Math.random() * n + 1));
   // console.log('Generated list', list);
@@ -13,7 +15,7 @@ const niceFunction = (n = 1000000) => {
     return list.filter(v2 => v2 === v1).length > 1
   });
 }
-const elementsThatRepeat = niceFunction(100000);
+const elementsThatRepeat = niceFunction(n);
 t = process.hrtime(t);
 console.log(`Execution time is ${t[0] + (~~(t[1] / 1000000)/1000)} seconds`)
-console.log('******************\n', elementsThatRepeat.length);
+console.log('*************************************\nNumber of elements that repeat: %s', elementsThatRepeat.length);
