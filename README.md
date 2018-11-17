e3f174ad46561d53b82afd75c7bfffaefba42e08
Marian Zburlea <marianzburlea@gmail.com>
2018-11-17 01:04:51 +0000

    improve search algorithm and arr readme file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Print list of duplicates
+
+To run it from the CLI do:
+```
+node nice.js n=12345
+```
+
+Run script with filter ***indexOf*** and ***lastIndexOf***
+```
+node nice.js n=100000
+Execution time is 5.4303 seconds
+*************************************
+Number of elements that repeat: 26389
+```
+
+Run script with filter algorithm
+```
+node nice.js n=100000
+Execution time is 22.9527 seconds
+*************************************
+Number of elements that repeat: 26453
+```
--- nice.js
+++ nice.js
@@ -7,22 +7,14 @@ let t = process.hrtime();
 
 const niceFunction = (n = 1000000) => {
   const list = Array(n + 1).fill('').map(v => ~~(Math.random() * n + 1));
-
-  // Using for takes a lot of time, bleahhhh!
-  // const uv = [];
-  // var i = 0,
-  //     ll = list.length;
-  // for (i; i < ll; i++) {
-  //   if (!uv.includes(list[i])) {
-  //     uv.push(list[i]);
-  //   }
-  // }
-
   const uniqueValues = [...(new Set(list))];
 
-  return true;
   return uniqueValues.filter(v1 => {
-    // console.log(list.filter(v2 => v2 === v1))
+    // indexOf and lastIndexOf algorithm
+    const io = list.indexOf(v1),
+          lio = list.lastIndexOf(v1);
+    return io > 0 && io != lio;
+    // Filter algorithm
     return list.filter(v2 => v2 === v1).length > 1
   });
 }
