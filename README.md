5423124d3892b955cec059f1cb9e3cfa8c5a0ef5
Marian Zburlea <marianzburlea@gmail.com>
2018-11-17 02:10:58 +0000

    Run script with print set to true

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ node nice.js n=12345
 
 ![image](https://user-images.githubusercontent.com/1318985/48655258-a1ceec80-ea0c-11e8-9b9a-e7793384855a.png)
 
+Run script with print set to true
+```
+ node nice.js n=67 print=true
+Execution time is 0.0004 seconds
+*************************************
+Number of elements that repeat: 19
+2,10,12,14,17,20,24,36,39,40,41,43,44,47,50,53,59,61,67
+```
+
 Run script with Object keys algorithm
 ```
 node nice.js n=100000
--- nice.js
+++ nice.js
@@ -1,6 +1,7 @@
 const getListOfDuplicates = require('./get-list-of-duplicates');
 // process.stdout.write('\033c');
 const n = process.argv && process.argv[2] && +process.argv[2].split('=')[1] || 1234;
+const print = process.argv && process.argv[3] && process.argv[3].split('=')[1] || false;
 let t = process.hrtime();
 
 const elementsThatRepeat = getListOfDuplicates.getListOfDuplicatesUsingObject(n);
@@ -8,3 +9,6 @@ const elementsThatRepeat = getListOfDuplicates.getListOfDuplicatesUsingObject(n)
 t = process.hrtime(t);
 console.log(`Execution time is ${t[0] + (~~(t[1] / 100000)/10000)} seconds`)
 console.log('*************************************\nNumber of elements that repeat: %s', elementsThatRepeat.length);
+
+// Print elements
+elementsThatRepeat && console.log(elementsThatRepeat.toString());
