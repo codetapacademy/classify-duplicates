d60f7d445c536862236629e8d955f3f6a9a513b9
Marian Zburlea <marianzburlea@gmail.com>
2018-11-17 01:55:19 +0000

    refactor to show better the evolution

--- get-list-of-duplicates.js
+++ get-list-of-duplicates.js
@@ -1,6 +1,16 @@
 // I want to build a function that returns an array 
 // with N + 1 elements that are random and integers
-const getListOfDuplicates = (n = 1000000) => {
+const getListOfDuplicatesUsingFilter = (n = 1000000) => {
+  const list = Array(n + 1).fill('').map(v => ~~(Math.random() * n + 1));
+  const uniqueValues = [...(new Set(list))];
+
+  return uniqueValues.filter(v1 => {
+    // Filter algorithm
+    return list.filter(v2 => v2 === v1).length > 1
+  });
+}
+
+const getListOfDuplicatesUsingIndex = (n = 1000000) => {
   const list = Array(n + 1).fill('').map(v => ~~(Math.random() * n + 1));
   const uniqueValues = [...(new Set(list))];
 
@@ -9,9 +19,29 @@ const getListOfDuplicates = (n = 1000000) => {
     const io = list.indexOf(v1),
           lio = list.lastIndexOf(v1);
     return io > 0 && io != lio;
-    // Filter algorithm
-    // return list.filter(v2 => v2 === v1).length > 1
   });
 }
 
-module.exports.getListOfDuplicates = getListOfDuplicates;
+// I want to build a function that returns an array 
+// with N + 1 elements that are random and integers
+const getListOfDuplicatesUsingObject = (n = 10000) => {
+  let length = n + 1,
+      i = 0,
+      o = {};
+  for(i; i < length; i++) {
+    const value = ~~(Math.random() * length);
+    if (o.hasOwnProperty(value)) {
+      o[value]++;
+    } else {
+      o[value] = 1;
+    }
+  }
+
+  return Object.keys(o).filter(key => o[key] > 1);
+}
+
+module.exports = {
+  getListOfDuplicatesUsingObject,
+  getListOfDuplicatesUsingFilter,
+  getListOfDuplicatesUsingIndex
+};
--- nice.js
+++ nice.js
@@ -1,11 +1,9 @@
-const getListOfDuplicates = require('./get-list-of-duplicates-2').getListOfDuplicates;
-// const getListOfDuplicates = require('./get-list-of-duplicates').getListOfDuplicates;
-
-// process.stdout.write('\033c');
+const getListOfDuplicates = require('./get-list-of-duplicates');
+process.stdout.write('\033c');
 const n = process.argv && process.argv[2] && +process.argv[2].split('=')[1] || 1234;
 let t = process.hrtime();
 
-const elementsThatRepeat = getListOfDuplicates(n);
+const elementsThatRepeat = getListOfDuplicates.getListOfDuplicatesUsingObject(n);
 
 t = process.hrtime(t);
 console.log(`Execution time is ${t[0] + (~~(t[1] / 100000)/10000)} seconds`)
